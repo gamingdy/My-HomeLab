cf0e78ce8183ec5971ab331ee49ed3af370d3418
gamingdy <christgik12@gmail.com>
2026-04-26 20:09:16 +0200

    [docs] feat: added collapsible to Code block

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,9 +1,12 @@
 import {pluginLineNumbers} from "@expressive-code/plugin-line-numbers";
+import {pluginCollapsibleSections} from '@expressive-code/plugin-collapsible-sections'
+
 
 /** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
 export default {
-    plugins: [pluginLineNumbers()],
+    plugins: [pluginLineNumbers(),pluginCollapsibleSections()],
     defaultProps: {
         showLineNumbers: true,
+        collapseStyle: 'collapsible-start',
     },
 };
